e9fe09d4a83276f6ee37c118364a72a53d532e7d
SYEUNG114 <108330403+SYEUNG114@users.noreply.github.com>
2022-11-19 20:29:10 +0800

    Add files via upload

---
+++ functions/floyd_imperative.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import itertools
+
+"""This is imperative version of Floyd's algorithm given by the assignment"""
+
+def floyd(distance):
+    MAX_LENGTH = len(distance)
+    for intermediate, start_node,end_node    in itertools.product    (range(MAX_LENGTH),range(MAX_LENGTH), range(MAX_LENGTH)):
+        # Assume that if start_node and end_node are the same
+        # then the distance would be zero
+        if start_node == end_node:
+            distance[start_node][end_node] = 0
+            continue
+        #return all possible paths and find the minimum
+        distance[start_node][end_node] = min(distance[start_node][end_node],
+        distance[start_node][intermediate] + distance[intermediate][end_node] )
+    return distance
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
